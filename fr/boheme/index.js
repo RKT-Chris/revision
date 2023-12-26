84201ff10ed254d0c4a111fd976f778d37e084e7
Rakotohasy Chris <chrisrakotohasy@gmail.com>
2023-12-26 21:40:06 +0100

    change to comma
    I changed the csv file from semi colon to comma

--- fr/boheme/index.js
+++ fr/boheme/index.js
@@ -1,1 +1,6 @@
-const mots = document.getElementsByTagName("span");
+d3.csv(
+  "https://github.com/RKT-Chris/revision/blob/79c7c407dd0ec0722804ed7b330b8f59bb7a13e1/explication.csv",
+  function (data) {
+    console.log(data);
+  }
+);
